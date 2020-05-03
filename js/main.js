e25abc066fc8cfa60f732b0cd96d00fe3540439b
Andreas Unger <andreas82un@web.de>
2020-05-03 21:44:32 +0200

    slider

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(function () {
+
+    // Menu opener hamburger
+    $('.menu-open, .menu a').click(function () {
+        $('.menu-collapse').toggleClass('d-none').css('order', '1');
+        $('.menu').toggleClass('menu-opened');
+    });
+
+
+    // Preloader
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+
+    // плавное перемещение страницы к нужному блоку
+    $("a.go").click(function (e) {
+        e.preventDefault();
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top;
+        $("body,html").animate({scrollTop: destination }, 800);
+    });
+
+
+
+    // Fixed navigation
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1200){  
+            $('nav').addClass("sticky");
+        }
+        else{
+            $('nav').removeClass("sticky");
+        }
+    });
+
+    // Всплывающее окно
+
+    $('.parallax-btn').click(function (e) {
+        e.preventDefault();
+        $('#exampleModal').arcticmodal();
+    });
+
+    // Большой слайдер
+    
+    $('.big-slider').slick({
+        dots: true,
+        infinite: false,
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+    });
+
+    // Большой слайдер
+    
+    $('.car-slider').slick({
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-arrow-alt-circle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-alt-circle-right"></i></button>',
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3
+          }
+        },   
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            arrows: false
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+
+    
+});
